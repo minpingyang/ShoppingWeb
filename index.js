8e137bb590ebf64bb416c8d8952849a9cd98b817
tongdarr <tong.darren1997@gmail.com>
2018-10-03 16:24:03 +1300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+var val = 0;
+automaticTransition(val);
+
+function automaticTransition(val){
+    var slides = document.getElementsByClassName("slideshow-imgs");
+    var dots = document.getElementsByClassName("dot");
+
+    // changes which dot is active, active dot is white and dark border, inactive is grey
+    for (i = 0; i < dots.length; i++) {
+      if(i === val){
+        dots[i].className = dots[i].className.replace(" inactiveDot", " activeDot");
+      }
+      else{
+        dots[i].className = dots[i].className.replace(" activeDot", " inactiveDot");
+      }
+    }
+
+    // hides all images by initially
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }  
+    
+    // set the image corresponding to the dot on display
+    slides[val].style.display = "block"; 
+
+    val++;
+
+    // reset the slides to beginning
+    if(val === slides.length){
+      val = 0;
+    }
+
+    setTimeout(automaticTransition, 2000);
+}
+
+function changePic(n) {
+    
+    var slides = document.getElementsByClassName("slideshow-imgs");
+    var dots = document.getElementsByClassName("dot");
+
+    // changes which dot is active, active dot is white and dark border, inactive is grey
+    for (var i = 0; i < dots.length; i++) {
+      if(i === n){
+        dots[i].className = dots[i].className.replace(" inactiveDot", " activeDot");
+      }
+      else{
+        dots[i].className = dots[i].className.replace(" activeDot", " inactiveDot");
+      }
+    }
+
+    // hides all images by initially
+    for (var i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }  
+    
+    // set the image corresponding to the dot on display
+    slides[n].style.display = "block"; 
+}
