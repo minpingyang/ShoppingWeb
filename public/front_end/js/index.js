a710755422705b516f7a4796a66eab82a8176828
Minping <yangminp@myvuw.ac.nz>
2018-11-06 00:42:07 +1300

    d

--- public/front_end/js/index.js
+++ public/front_end/js/index.js
@@ -40,15 +40,15 @@ $(document).ready(function(e){
     //   $('#login').text("Log Out");
       
     // }
-    if(localStorage.getItem("email")!==null&&btn_content==="Log In"){
+    if(localStorage.getItem("email")!==null&&$('#login').text().trim()==="Log In"){
       console.log("22222");
       $('#login').text("Log Out");
       console.log("2.11111"+$('#login').text());
     }
   });
   $("#login").button().click(function(){
-    console.log("333"+btn_content);
-    if(btn_content==="Log Out"){
+    console.log("333"+$('#login').text().trim());
+    if($('#login').text().trim()==="Log Out"){
       console.log("4444");
       localStorage.clear();
       $('#login').text("Log In");
