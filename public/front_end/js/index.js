3d9ddacbd3c1c546552b4a46dcb4971d6eedc75c
Minping <yangminp@myvuw.ac.nz>
2018-11-09 15:51:44 +1300

    d

--- public/front_end/js/index.js
+++ public/front_end/js/index.js
@@ -32,6 +32,11 @@ $(document).ready(function(e){
   var localStorage = window.localStorage;
   console.log("storage: "+localStorage.getItem("email"));
   $(function () {
+    if(localStorage.getItem("google")==="true"){
+      $('#login_google').text("Log Out Gooogle");
+    }else if(localStorage.getItem("google")==="false"){
+      $('#login_google').text("Log In by Gooogle");
+    }
     if(localStorage.getItem("email")!==null&&$('#login').text().trim()==="Log In"){
       console.log("22222");
       $('#login').text("Log Out");
@@ -57,16 +62,17 @@ $(document).ready(function(e){
     console.log("333"+$('#login_google').text().trim());
     if($('#login_google').text().trim()==="Log Out Gooogle"){
       console.log("4444");
-      
-      $('#login_google').text("Log In");
+      var ifGoogle= window.localStorage;
+      ifGoogle.setItem("google","false");
+      // $('#login_google').text("Log In");
       window.location.href="/logout_google";
      
     }
     else{
     console.log("5555");
-     alert("go to ")
-    //  window.open("/html/login.html");
-      $('#login_google').text("Log Out Gooogle");
+     alert("go to ");
+      var ifGoogle= window.localStorage;
+      ifGoogle.setItem("google","true");
       window.location.href="/login_google";
     }
   });
