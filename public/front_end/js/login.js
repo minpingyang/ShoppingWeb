8858606177bd50ffc3949850dfdad377679c37ab
Minping <yangminp@myvuw.ac.nz>
2018-11-12 20:00:20 +1300

    d

--- public/front_end/js/login.js
+++ public/front_end/js/login.js
@@ -8,27 +8,45 @@ $(document).ready(function (e) {
 	var localStorage = window.localStorage;
 	console.log("storage: " + localStorage.getItem("email"));
 	$(function () {
-		if (localStorage.getItem("email") !== null && $('#login').text().trim() === "Log In") {
+		if (sessionGet("email") !== null && $('#login').text().trim() === "Log In") {
 			console.log("log in page dectected");
 			$('#login').text("Log Out");
 			console.log("2.11111" + $('#login').text());
 		}
 	});
 	$("#login").button().click(function () {
-		console.log("333" + $('#login').text().trim());
-		if ($('#login').text().trim() === "Log Out") {
-			console.log("4444");
-			localStorage.clear();
-			$('#login').text("Log In");
+    console.log("333" + $('#login').text().trim());
+    if ($('#login').text().trim() === "Log Out") {
+      console.log("4444");
+      // localStorage.clear();
+      sessionSet("email",null);
+      $('#login').text("Log In");
 
-		}
-		else {
-			console.log("5555");
-			alert("go to ")
-			//  window.open("/html/login.html");
-			window.location.href = "login.html";
-		}
-	});
+    }
+    else {
+      console.log("5555");
+      alert("go to ")
+      //  window.open("/html/login.html");
+      window.location.href = "login.html";
+    }
+  });
+
+	$("#login").button().click(function () {
+    console.log("333" + $('#login').text().trim());
+    if ($('#login').text().trim() === "Log Out") {
+      console.log("4444");
+      // localStorage.clear();
+      sessionSet("email",null);
+      $('#login').text("Log In");
+
+    }
+    else {
+      console.log("5555");
+      alert("go to ")
+      //  window.open("/html/login.html");
+      window.location.href = "html/login.html";
+    }
+  });
 	function sessionGet(key) {
 		let stringValue = window.sessionStorage.getItem(key)
 		if (stringValue !== null) {
