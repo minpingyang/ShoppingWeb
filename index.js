0d24251c0c9a21a8ecca99adcf1e011a8fd05465
Darren Tong <tong.darren1997@gmail.com>
2018-11-13 17:21:39 +1300

    Asd

--- index.js
+++ index.js
@@ -378,27 +378,13 @@ app.post('/order', async (req, res) => {
     var price = req.body.price;
     var quantity = req.body.quantity;
 
-    var query_state = "insert into ";
+    var query_state = "insert into orders(item_name, price, quantity, status) values('" +  + "'" ")";
     console.log(query_state);
     var result = await client.query(query_state);
     var json = [];
 
     // generate json string containing img path, item name, price, quantity
-    result.rows.forEach(async row=>{
-      var id = row.item_id;
-
-      var query_state2 = "select * from items where item_id = " + id;
-      console.log(query_state2);
-      var result2 = await client.query(query_state2);
-
-      row["item_name"] = result2.rows[0].item_name; 
-      row["img"] = result2.rows[0].img;
-      row["price"] = result2.rows[0].price;
-      json.push(row);
-      if(json.length == result.rowCount){
-          return res.send(json);
-      }
-    });
+    
 
   } catch (err) {
     console.error(err);
@@ -433,7 +419,8 @@ app.post('/view_cart', async (req, res) => {
           return res.send(json);
       }
     });
-
+    //asd
+    
   } catch (err) {
     console.error(err);
     res.send("Error " + err);
